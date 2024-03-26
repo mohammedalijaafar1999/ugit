0f07e5ea7015db53e60df4ab088c9c7b6cc46bb4
Mohammed Ali <mohammedalijaafar1999@gmail.com>
2024-03-27 01:15:29 +0300

    added hashing to the mix

--- ugit/data.py
+++ ugit/data.py
@@ -1,6 +1,14 @@
+import hashlib
 import os
 
 GIT_DIR = '.ugit'
 
 def init ():
     os.makedirs (GIT_DIR)
+    os.makedirs (f'{GIT_DIR}/objects')
+
+def hash_object (data):
+    oid = hashlib.sha1 (data).hexdigest ()
+    with open (f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write (data)
+    return oid
