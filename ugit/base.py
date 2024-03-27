1b437a4d8e4560c94ff0100096acf0646876c790
Mohammed Ali <mohammedalijaafar1999@gmail.com>
2024-03-27 20:52:39 +0300

    write-tree: Ignore .ugit files, and refactored the code so that it is Compatible with both Unix and Windows systems

--- ugit/base.py
+++ ugit/base.py
@@ -1,16 +1,20 @@
 import os
-
 from . import data
 
-
-def write_tree (directory='.'):
-    with os.scandir (directory) as it:
+def write_tree(directory='.'):
+    with os.scandir(directory) as it:
         for entry in it:
-            full = f'{directory}/{entry.name}'
-            if entry.is_file (follow_symlinks=False):
+            full = os.path.join(directory, entry.name)
+            if is_ignored(full):   
+                continue
+
+            if entry.is_file(follow_symlinks=False):
                 # TODO write the file to object store
-                print (full)
-            elif entry.is_dir (follow_symlinks=False):
-                write_tree (full)
+                print(full)
+            elif entry.is_dir(follow_symlinks=False):
+                write_tree(full)
 
     # TODO actually create the tree object
+                
+def is_ignored(path):
+    return '.ugit' in path.split(os.sep)
